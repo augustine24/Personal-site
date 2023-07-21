c3cc03f0da72b2a89e37b9178e5523d5c32df2c4
Your Name <106200215+augustine24@users.noreply.github.com>
2023-07-20 20:04:53 -0400

    fixes
    fix

--- myblog/views.py
+++ myblog/views.py
@@ -2,11 +2,10 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from .forms import ContactForm
 from django.core.mail import send_mail
 from django.conf import settings
-from django.http import HttpResponse
 
 
 class HomeView(ListView):
